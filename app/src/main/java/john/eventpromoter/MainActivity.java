1740a5ec1bafa46126c364dc0ba9fd2e53dab6d7
GreatImNate <nathan.seegmiller@utexas.edu>
2017-04-04 18:28:41 -0500

    Added an information dialog to explain what the app does, button will probably be replaced with settings and the about will be in the settings

--- app/src/main/java/john/eventpromoter/MainActivity.java
+++ app/src/main/java/john/eventpromoter/MainActivity.java
@@ -1,11 +1,13 @@
 package john.eventpromoter;
 
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.firebase.database.DataSnapshot;
@@ -62,7 +64,21 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        FragmentManager fragmentManager = getFragmentManager();
+        switch (item.getItemId()){
+            case R.id.information:
+                AppDescriptionDialogFragment dialogFragment = new AppDescriptionDialogFragment();
+                dialogFragment.show(fragmentManager, "info");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
